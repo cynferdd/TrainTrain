940adaca752ba89a7dba319dc08b8349b24ccd0e
Christophe Moinard <cmoinard@lucca.fr>
2020-11-16 17:51:49 +0100

    Refactored with custom context

---
+++ TrainTrain.Domain.Tests/Contexts/AddContext.cs
@@ -0,0 +1,9 @@
+namespace TrainTrain.Domain.Tests.Contexts
+{
+    public class AddContext
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+        public int Actual { get; set; }
+    }
+}
--- TrainTrain.Domain.Tests/Steps/AddSteps.cs
+++ TrainTrain.Domain.Tests/Steps/AddSteps.cs
@@ -1,4 +1,5 @@
 using TechTalk.SpecFlow;
+using TrainTrain.Domain.Tests.Contexts;
 using Xunit;
 
 namespace TrainTrain.Domain.Tests.Steps
@@ -6,24 +7,25 @@ namespace TrainTrain.Domain.Tests.Steps
     [Binding]
     public class AddSteps
     {
-        private int _first;
-        private int _second;
-        private int _actual;
+        private readonly AddContext _context;
+
+        public AddSteps(AddContext context) => 
+            _context = context;
 
         [Given(@"un premier nombre (\d+)")]
         public void AddFirstNumber(int number) => 
-            _first = number;
+            _context.First = number;
 
         [Given(@"un deuxième nombre (\d+)")]
         public void AddSecondNumber(int number) => 
-            _second = number;
+            _context.Second = number;
 
         [When(@"on les additionne")]
         public void AddTwoNumbers() => 
-            _actual = _first + _second;
+            _context.Actual = _context.First + _context.Second;
 
         [Then(@"le résultat est (\d+)")]
         public void TheResultShouldBe(int expected) =>
-            Assert.Equal(expected, _actual);
+            Assert.Equal(expected, _context.Actual);
     }
 }
