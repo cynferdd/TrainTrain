904fc9a174d27c5d5cb2bcdbe284a3047f474445
Christophe Moinard <cmoinard@netc.fr>
2020-11-17 23:17:41 +0100

    Dépassement de seuil pour une réservation de groupe

--- TrainTrain.Domain/RéservationService.cs
+++ TrainTrain.Domain/RéservationService.cs
@@ -2,11 +2,12 @@ namespace TrainTrain.Domain
 {
     public class ReservationService
     {
+        private const decimal SeuilDeReservation = 70m;
         private const decimal Prix = 50m;
 
         public decimal? Reserver(Wagon wagon, int nbPlaces)
         {
-            if (wagon.NbPlacesOccupees == 70m)
+            if (SeuilDeReservation < wagon.NbPlacesOccupees + nbPlaces)
                 return null;
 
             wagon.Reserver(nbPlaces);
