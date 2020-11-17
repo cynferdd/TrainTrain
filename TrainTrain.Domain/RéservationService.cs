505763ef99d236d28884d2af5bc277e65b0b5f73
Christophe Moinard <cmoinard@netc.fr>
2020-11-17 22:51:52 +0100

    Vrai prix pour la réservation

--- TrainTrain.Domain/RéservationService.cs
+++ TrainTrain.Domain/RéservationService.cs
@@ -2,11 +2,13 @@ namespace TrainTrain.Domain
 {
     public class ReservationService
     {
+        private const decimal Prix = 50m;
+
         public decimal? Réserver(Wagon wagon, int nbPlaces)
         {
             return
                 wagon.Réserver(nbPlaces)
-                    ? 56756757657m
+                    ? Prix
                     : (decimal?)null;
         }
     }
